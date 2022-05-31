1dcdb3885186845f7c7c5ef9fa84fa2a856209f4
Neil Juanillo <37945822+ajax12233512@users.noreply.github.com>
2022-05-31 11:30:47 -0500

    added hero content

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Hero from './components/Hero/Hero';
 import Navbar from './components/Navbar/Navbar';
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Hero />
     </div>
   );
 }
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import Button from '../Button/Button'
+import heroPic from '../../images/image-hero-desktop.png'
 import './Hero.css'
+import {ReactComponent as Databiz} from '../../images/client-databiz.svg'
+import {ReactComponent as Audiophile} from '../../images/client-audiophile.svg'
+import {ReactComponent as Maker} from '../../images/client-maker.svg'
+import {ReactComponent as Meet} from '../../images/client-meet.svg'
 
 function Hero() {
   return (
-    <div>Hero</div>
+    <div className='hero'>
+      <div className='hero-left'>
+        <h1>Make remote work</h1>
+        <p>Get you team in sync, no matter your location. Streamline processes, create team rituals, and watch productivity soar.</p>
+        <Button text='Learn more' />
+        <div className='hero-left-sponsers'>
+          <Databiz />
+          <Audiophile />
+          <Maker />
+          <Meet />
+        </div>
+      </div>
+      <div className='hero-right'>
+        <img src={heroPic} alt='hero' />
+      </div>
+    </div>
   )
 }
 
