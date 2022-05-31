66edd8dd32a7a1f00d243a48d766b3fe537c1bc8
Neil Juanillo <37945822+ajax12233512@users.noreply.github.com>
2022-05-31 11:01:24 -0500

    navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './Navbar.css'
+import {ReactComponent as UpArrow} from '../../images/icon-arrow-up.svg'
+import {ReactComponent as DownArrow} from '../../images/icon-arrow-down.svg'
 import logo from '../../images/logo.svg'
 import navbarItems from './NavbarItems'
 function Navbar() {
@@ -10,10 +12,16 @@ function Navbar() {
         <img src={logo} alt='logo' />
       </div>
       {/* Navbar Items */}
-      <div className='navbar__items'>
-        {navbarItems.map(item => <div className='navbar__item'>{item.text}</div>)}
+      <div className='navbar__items' >
+        {navbarItems.map(item => <div className='navbar__item'>
+            {item.text}
+             <UpArrow />
+            {item?.subitems ? item.subitems.map(item => <p>{item}</p>) : null}
+          </div>)}
       </div>
-      <div className='navbar-btns'></div>
+      <div className='navbar-btns'>
+
+      </div>
     </div>
   )
 }
--- src/components/Navbar/NavbarItems.js
+++ src/components/Navbar/NavbarItems.js
@@ -1,15 +1,18 @@
 import arrowDown  from '../../images/icon-arrow-down.svg';
 import arrowUp  from '../../images/icon-arrow-up.svg';
+import { featuresSubitems, companySubitems } from './NavbarSubitems';
 const navbarItems = [
     {
         text: 'Features',
         link: '/features',
-        icon: [<i>{arrowDown}</i>, <i>{arrowUp}</i>]
+        icon: [<i>{arrowDown}</i>, <i>{arrowUp}</i>],
+        subitems: featuresSubitems
     },
     {
         text: 'Company',
         link: '/company',
-        icon: [<i>{arrowDown}</i>, <i>{arrowUp}</i>]
+        icon: [<i>{arrowDown}</i>, <i>{arrowUp}</i>],
+        subitems: companySubitems
     },
     {
         text: 'Careers',
@@ -22,5 +25,7 @@ const navbarItems = [
         icon: [<i>{arrowDown}</i>, <i>{arrowUp}</i>]
     },
 ]
+
+
 
 export default navbarItems;
