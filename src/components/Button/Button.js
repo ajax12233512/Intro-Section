fd5ee8894c63a18c22c136322872e41775d8578d
Neil Juanillo <37945822+ajax12233512@users.noreply.github.com>
2022-06-01 03:35:23 -0500

    added stuff

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,15 +2,24 @@ import React from 'react'
 import './Button.css'
 
 const Button = ({text, height}) => {
-
+  
+  const containerStyle = {
+      width: '50px',
+  }  
+  
   const style = {
-    backgroundColor: 'black',
+    backgroundColor: 'white',
     height: height,
-    color: 'white'
+    color: 'black',
+    borderRadius: '5px',
+    border: '1px solid black',
+    width: '100%'
   }
 
   return (
+      <div style={containerStyle}>
         <button style={style} className="Button">{text}</button>
+      </div>
   )
 }
 
--- src/components/Navbar/NavbarSubItems.jsx
+++ src/components/Navbar/NavbarSubItems.jsx
@@ -3,8 +3,11 @@ import React from 'react'
 const style = {
     margin: '1rem',
     top: '0px',
+    right: '20px',
     position: 'relative',
     backgroundColor: '#f5f5f5',
+    width: '100px',
+    zIndex: '1'
 }
 function NavbarSubItems(props) {
   return (
